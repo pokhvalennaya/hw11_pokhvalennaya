35c428d153e87e4db53ded25969de52b06e24b8d
Lena <elena.pokhvalennaya@gmail.com>
2022-03-30 12:38:54 +0300

    update task

--- script.js
+++ script.js
@@ -11,27 +11,42 @@ class Person {
 }
 
 class Employee extends Person {
-  constructor(firstName, lastName, age, birthDayDate, jobPosition) {
+  #salary;
+  constructor(firstName, lastName, age, birthDayDate, salary, jobPosition) {
     super(firstName, lastName, age, birthDayDate);
+    this.#salary = salary;
     this.jobPosition = jobPosition;
   }
-  #salary = 1000;
+
   getYearSalary() {
     return this.#salary * 12;
   }
   celebrate() {
     let thisYear = new Date().getFullYear();
     let birthdayThisYear = new Date(this.birthDayDate).setFullYear(thisYear);
-    let dayOfWeek = new Date(birthdayThisYear).getDay();
-    if (dayOfWeek == 0 || dayOfWeek == 6) {
-      return "Happy Birthday, let`s celebrate";
+    if (isWeekend(birthdayThisYear) === true) {
+      return super.celebrate();
     } else {
       return "Happy Birthday, but I need to work";
     }
   }
 }
+
+function isWeekend(date) {
+  let dayOfWeek = new Date(date).getDay();
+  let weekend = dayOfWeek == 0 || dayOfWeek == 6;
+  return weekend;
+}
+
 const person = new Person("Lena", "Pokhvalennaya", "19", "2002-05-22");
-const employee = new Employee("Sofia", "Serdiuk", "20", "2001-12-14", "DevOps");
+const employee = new Employee(
+  "Sofia",
+  "Serdiuk",
+  "20",
+  "2001-12-14",
+  1000,
+  "DevOps"
+);
 
 console.log(person.celebrate());
 console.log(employee.celebrate());
